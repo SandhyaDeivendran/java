c55be3e57747a81f3e3db6761eb19b8aba4675c1
Sandhya Deivendran <sandhyad23ec@psnacet.edu.in>
2025-08-06 09:31:45 +0530

    Create arr2d1.java
    100
    010
    001

---
+++ arr2d1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+        int columns=sc.nextInt();
+        int [][] arr=new int[rows][columns];
+        if(rows==columns){
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr.length;j++){
+                     if(i==j){
+                          arr[i][j]=1;
+                     }
+                     else{
+                          arr[i][j]=0;
+                     }
+                    
+                }
+            }
+        }
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr.length;j++){
+                     System.out.print(arr[i][j]+" ");
+                     
+                     
+                } 
+                System.out.println(" ");
+    }
+}
+}
