b902d0da5095bc3a6cbba8aa8485426d3c2b1606
Sandhya Deivendran <sandhyad23ec@psnacet.edu.in>
2025-08-06 10:17:37 +0530

    Create inputarr.java
    1 2 3 4
    5 6 7 8
    9 10 11 12
    13 14 15 16
    for start from 10 change a and a+=

---
+++ inputarr.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=1;
+        int b=0;
+        int rows=sc.nextInt();
+        int columns=sc.nextInt();
+        int [][] arr= new int[rows][columns]; 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+          
+                arr[i][j]=a;
+                a+=1;
+                b+=a;
+            }
+        }
+        System.out.println(b);
+
+        for (int i = 0; i <rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+    }
+}
